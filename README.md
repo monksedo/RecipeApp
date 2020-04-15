c920599bf343102fa8d9e60c33aaf7fe8902c79a
Chris Nguyen <chris@tnwebdev.com>
2020-04-14 21:47:11 -0700

    Update code base with addition features

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Food Recipe Search App   
+# FOOD RECIPES SEARCH APP   
 A homework project that using all the JavaScript concepts learned in previous sections to create a Recipe Search App using JavaScript ES6 features, HTML, and CSS.
 
 ## Run Test
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,24 +1,40 @@
 import { elements } from './base';
 
-export const getInput = () => elements.searchInput.value;
+// Read the input field value 
+const getInput = () => elements.searchInput.value;
 
-export const renderRecipe = recipe => {
-   const markup = `
-   <li>
-   <a class="likes__link" href="#${recipe.recipe_id}">
-   <figure class="likes__fig">
-   <img src="${recipe.image_url}" alt="${recipe.title}">
-   </figure>
-   <div class="likes__data">
-   <h4 class="likes__name">${recipe.title}</h4>
-   <p class="likes__author">${recipe.publisher}</p>
-   </div>
-   </a>
-   </li>
-   `;
-   elements.resultList.insertAdjacentHTML('beforeend', markup);
+// Reset the input field 
+const clearInput = () => {
+   elements.searchInput.value = '';
+};
+
+// Remove the past result list
+const clearResults = () => {
+   elements.resultList.innerHTML = '';
 }
 
-export const renderResults = recipes => {
+// Display results on UI
+const renderRecipe = recipe => {
+   const markup = `
+      <li>
+         <a class="likes__link" href="#${recipe.recipe_id}">
+            <figure class="likes__fig">
+               <img src="${recipe.image_url}" alt="${recipe.title}">
+            </figure>
+            <div class="likes__data">
+               <h4 class="likes__name">${recipe.title}</h4>
+               <p class="likes__author">${recipe.publisher}</p>
+            </div>
+         </a>
+      </li>
+   `;
+   elements.resultList.insertAdjacentHTML('beforeend', markup);
+};
+
+// Iterate though result list 
+const renderResults = recipes => {
    recipes.forEach(renderRecipe);
 };
+
+// Make function available to external modules
+export { getInput, renderResults, clearInput, clearResults };
